580130a3f4ed200e6598fa92067b64f151e77819
David Schmid <david.schmid@mailbox.org>
2023-12-11 21:42:01 +0100

    docs(aes_ecb): Add documentation for AES ECB mode encryption and decryption
    - Add detailed module-level documentation explaining AES ECB mode and its
      features
    - Include usage examples and a disclaimer about the limitations of ECB mode
    - Add doc tests to demonstrate encryption and decryption using AES-128 in ECB mode

--- src/aes/aes_ecb.rs
+++ src/aes/aes_ecb.rs
@@ -1,3 +1,60 @@
+//! AES Encryption and Decryption in ECB Mode
+//!
+//! This module provides functionality for encrypting and decrypting data using
+//! the Advanced Encryption Standard (AES) in Electronic Codebook (ECB) mode.
+//! It includes support for optional padding, specifically PKCS#7 padding, to
+//! accommodate data that does not align with the AES block size.
+//!
+//! ECB mode operates on fixed-size blocks of data and is one of the simplest
+//! encryption modes. While it is not recommended for encrypting large volumes
+//! of data or data with patterns due to security concerns, it remains useful
+//! for certain applications.
+//!
+//! # Features
+//!
+//! - `aes_enc_ecb`: Encrypts data using AES in ECB mode. It supports optional
+//!   PKCS#7 padding for data that is not a multiple of the AES block size.
+//!
+//! - `aes_dec_ecb`: Decrypts data that was encrypted using AES in ECB mode.
+//!   It also supports the removal of PKCS#7 padding if it was applied during
+//!   encryption.
+//!
+//! The implementation assumes that the provided key is of a valid length for
+//! AES (128, 192, or 256 bits). The module integrates closely with the core
+//! AES functionalities and the PKCS#7 padding module to offer a seamless
+//! encryption/decryption experience.
+//!
+//! # Usage
+//!
+//! This module is suitable for scenarios where simple, block-wise encryption
+//! and decryption are needed without the complexities of more advanced modes
+//! like CBC or CTR. It is especially useful in contexts where data patterns
+//! are not a concern, or where the simplicity of ECB mode is a key requirement.
+//!
+//! # Example
+//!
+//! Basic example of encrypting and decrypting data using AES-128 in ECB mode:
+//!
+//! ```
+//! use crate::soft_aes::aes::{aes_enc_ecb, aes_dec_ecb};
+//!
+//! let plaintext = b"Example plaintext.";
+//! let key = b"Very secret key.";
+//! let padding = Some("PKCS7");
+//!
+//! let encrypted = aes_enc_ecb(plaintext, key, padding).expect("Encryption failed");
+//! let decrypted = aes_dec_ecb(&encrypted, key, padding).expect("Decryption failed");
+//!
+//! assert_eq!(decrypted, plaintext);
+//! ```
+//!
+//! # Disclaimer
+//!
+//! - ECB mode does not provide serious confidentiality in many cases, as it
+//!   does not use an initialization vector (IV) and encrypts identical plaintext
+//!   blocks into identical ciphertext blocks. It should be used with caution,
+//!   especially for encrypting data with repetitive patterns.
+
 use super::super::padding::*;
 use super::aes_core::*;
 

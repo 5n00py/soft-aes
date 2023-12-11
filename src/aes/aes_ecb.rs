876aa845a6dc7c4f887583785387d19be96bb3fe
David Schmid <david.schmid@mailbox.org>
2023-12-11 21:40:46 +0100

    feat(aes_ecb): Add aes_dec_ecb function
    - Implement aes_dec_ecb function to decrypt data using AES in ECB mode.
    - Check if ciphertext length is a multiple of AES_BLOCK_SIZE.
    - Decrypt each block of the ciphertext.
    - Remove PKCS7 padding if it was used during encryption.

--- src/aes/aes_ecb.rs
+++ src/aes/aes_ecb.rs
@@ -43,3 +43,45 @@ pub fn aes_enc_ecb(
 
     Ok(ciphertext)
 }
+
+/// Decrypt data using AES in ECB mode with optional padding removal.
+///
+/// # Parameters
+/// - `ciphertext`: The encrypted data to decrypt. It should be a multiple of
+///                 `AES_BLOCK_SIZE`.
+/// - `key`: The decryption key.
+/// - `padding`: Optional padding method used during encryption. Supported value
+///              is `PKCS7` for removing padding after decryption.
+///
+/// # Returns
+/// Returns a `Result<Vec<u8>, Box<dyn std::error::Error>>` containing the
+/// decrypted data or an error.
+pub fn aes_dec_ecb(
+    ciphertext: &[u8],
+    key: &[u8],
+    padding: Option<&str>,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    if ciphertext.len() % AES_BLOCK_SIZE != 0 {
+        return Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "AES DEC ECB Error: Ciphertext must be a multiple of AES_BLOCK_SIZE",
+        )));
+    }
+
+    let mut plaintext = Vec::with_capacity(ciphertext.len());
+
+    // Decrypt each block
+    for block in ciphertext.chunks(AES_BLOCK_SIZE) {
+        let mut block_array = [0u8; AES_BLOCK_SIZE];
+        block_array.copy_from_slice(block);
+        let decrypted_block = aes_dec_block(&block_array, key)?;
+        plaintext.extend_from_slice(&decrypted_block);
+    }
+
+    // Remove PKCS7 padding if it was used during encryption
+    if let Some("PKCS7") = padding {
+        pkcs7_unpad(&mut plaintext)?;
+    }
+
+    Ok(plaintext)
+}
